ebd30886a8db35d2d09884e2dd83de927b41bb53
Wolfram Kriesing <wk@uxebu.com>
2014-04-01 15:01:45 +0200

    Some example code

---
+++ src/introduction/js-basics.js
@@ -0,0 +1,73 @@
+;(function(){
+
+
+  function getWindowObject(window) {
+    if (typeof window == 'undefined') {
+      var window = new FakeWindow();
+    }
+    return window;
+  }
+
+
+  // simple constructs
+
+  (function(){})();
+
+  (function(){
+
+    var x = 'outer';
+    (function(){
+
+      var x = 'inner';
+
+    })();
+    console.log(x);
+
+  })();
+
+
+  (function(){
+
+    var x = 'outer';
+    (function(){
+
+      x = 'inner';
+
+    })();
+    console.log(x);
+
+  })();
+
+  var x;
+
+  x = '4' + 4;
+
+  x = 4 + '4';
+
+  x = 4 - '4';
+
+  x = 4 * '4';
+
+  x = 4 + false;
+
+  x = 4 - false;
+
+  x = false - 4;
+
+
+  x = 4 - {};
+
+  x = {} - 4;
+
+  x = 4 - [];
+
+  x = 4 - [1];
+
+  x = 4 - [1,2];
+
+  x = 4 - 1/0;
+
+  x = 040 + 4;
+
+
+})();
