6b7c762ff2de0b9af86629f8c8d5ded6a5fa52d8
Wolfram Kriesing <wk@uxebu.com>
2014-04-01 22:40:59 +0200

    Refactor into smaller functions.

--- src/refactoring.js
+++ src/refactoring.js
@@ -1,35 +1,51 @@
 // https://github.com/jquery/jquery/blob/master/src/manipulation.js#L89
 
-    function cloneCopyEvent( src, dest ) {
-      var i, l, type, pdataOld, pdataCur, udataOld, udataCur, events;
+function removeAllEvents(dest, pdataOld) {
+  var pdataCur = data_priv.set(dest, pdataOld);
+  delete pdataCur.handle;
+  pdataCur.events = {};
+}
 
-      if ( dest.nodeType !== 1 ) {
-        return;
-      }
+function addEvent(dest, type, event) {
+  var l = event.length;
+  for (var i = 0; i < l; i++) {
+    jQuery.event.add(dest, type, event[ i ]);
+  }
+  return i;
+}
 
-      // 1. Copy private data: events, handlers, etc.
-      if ( data_priv.hasData( src ) ) {
-        pdataOld = data_priv.access( src );
-        pdataCur = data_priv.set( dest, pdataOld );
-        events = pdataOld.events;
+function addAllDefinedEvents(events, dest) {
+  for (var type in events) {
+    var event = events[ type ];
+    addEvent(dest, type, event);
+  }
+}
 
-        if ( events ) {
-          delete pdataCur.handle;
-          pdataCur.events = {};
+function copyPrivateData(src, dest) {
+  var pdataOld = data_priv.access(src);
+  var events = pdataOld.events;
 
-          for ( type in events ) {
-            for ( i = 0, l = events[ type ].length; i < l; i++ ) {
-              jQuery.event.add( dest, type, events[ type ][ i ] );
-            }
-          }
-        }
-      }
+  if (events) {
+    removeAllEvents(dest, pdataOld);
+    addAllDefinedEvents(events, dest);
+  }
+}
 
-      // 2. Copy user data
-      if ( data_user.hasData( src ) ) {
-        udataOld = data_user.access( src );
-        udataCur = jQuery.extend( {}, udataOld );
+function copyUserData(src, dest) {
+  var udataOld = data_user.access(src);
+  var udataCur = jQuery.extend({}, udataOld);
 
-        data_user.set( dest, udataCur );
-      }
-    }
+  data_user.set(dest, udataCur);
+}
+
+function cloneCopyEvent(src, dest) {
+  if (dest.nodeType !== 1) {
+    return;
+  }
+  if (data_priv.hasData(src)) {
+    copyPrivateData(src, dest);
+  }
+  if (data_user.hasData(src)) {
+    copyUserData(src, dest);
+  }
+}
