a62e66a658f39369eb88421e48b554742825baa1
Wolfram Kriesing <wk@uxebu.com>
2014-04-02 10:26:17 +0200

    A bit moving around and renaming.

--- src/refactoring.js
+++ src/refactoring.js
@@ -1,51 +1,50 @@
 // https://github.com/jquery/jquery/blob/master/src/manipulation.js#L89
 
-function removeAllEvents(dest, pdataOld) {
-  var pdataCur = data_priv.set(dest, pdataOld);
-  delete pdataCur.handle;
-  pdataCur.events = {};
-}
-
-function addEvent(dest, type, event) {
-  var l = event.length;
-  for (var i = 0; i < l; i++) {
-    jQuery.event.add(dest, type, event[ i ]);
-  }
-  return i;
-}
-
-function addAllDefinedEvents(events, dest) {
-  for (var type in events) {
-    var event = events[ type ];
-    addEvent(dest, type, event);
-  }
-}
-
-function copyPrivateData(src, dest) {
-  var pdataOld = data_priv.access(src);
-  var events = pdataOld.events;
-
-  if (events) {
-    removeAllEvents(dest, pdataOld);
-    addAllDefinedEvents(events, dest);
-  }
-}
-
-function copyUserData(src, dest) {
-  var udataOld = data_user.access(src);
-  var udataCur = jQuery.extend({}, udataOld);
-
-  data_user.set(dest, udataCur);
-}
-
 function cloneCopyEvent(src, dest) {
   if (dest.nodeType !== 1) {
     return;
   }
   if (data_priv.hasData(src)) {
-    copyPrivateData(src, dest);
+    _copyPrivateData(src, dest);
   }
   if (data_user.hasData(src)) {
-    copyUserData(src, dest);
+    _copyUserData(src, dest);
   }
+}
+
+function _removeEvent(dest, pdataOld) {
+  var pdataCur = data_priv.set(dest, pdataOld);
+  delete pdataCur.handle;
+  pdataCur.events = {};
+}
+
+function _addEvent(dest, type, event) {
+  var l = event.length;
+  for (var i = 0; i < l; i++) {
+    jQuery.event.add(dest, type, event[ i ]);
+  }
+}
+
+function _addEvents(events, dest) {
+  for (var type in events) {
+    var event = events[ type ];
+    _addEvent(dest, type, event);
+  }
+}
+
+function _copyPrivateData(src, dest) {
+  var pdataOld = data_priv.access(src);
+  var events = pdataOld.events;
+
+  if (events) {
+    _removeEvent(dest, pdataOld);
+    _addEvents(events, dest);
+  }
+}
+
+function _copyUserData(src, dest) {
+  var udataOld = data_user.access(src);
+  var udataCur = jQuery.extend({}, udataOld);
+
+  data_user.set(dest, udataCur);
 }
