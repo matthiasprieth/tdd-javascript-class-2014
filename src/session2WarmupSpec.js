c0ca5a7305e2b073425f1742aed093b0788895a2
Wolfram Kriesing <wk@uxebu.com>
2014-04-03 10:16:48 +0200

    Implemented up to 50.

--- src/session2WarmupSpec.js
+++ src/session2WarmupSpec.js
@@ -11,9 +11,27 @@
       it('should convert I to 1', function() {
         expect(convert('I')).toBe(1);
       });
+      it('should convert V to 5', function() {
+        expect(convert('V')).toBe(5);
+      });
+      it('should convert X to 10', function() {
+        expect(convert('X')).toBe(10);
+      });
+      it('should convert L to 50', function() {
+        expect(convert('L')).toBe(50);
+      });
     });
 
-    var convert = function() {
+    var convert = function(arabic) {
+      if (arabic == 'V') {
+        return 5;
+      }
+      if (arabic == 'X') {
+        return 10;
+      }
+      if (arabic == 'L') {
+        return 50;
+      }
       return 1;
     };
 
