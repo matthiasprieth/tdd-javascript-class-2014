f3ddf69d1753dd0509e142ffee24a855850eb278
Wolfram Kriesing <wk@uxebu.com>
2014-04-08 22:44:27 +0200

    Some slide mocks.

---
+++ src/jQueryImageMock.js
@@ -0,0 +1,55 @@
+
+
+
+  function showAndRotateImage(imageId, angle) {
+    var image = jQuery('#' + imageId);
+    image.show();
+    image.css('webkitTransform', 'rotate(' + angle + 'deg)');
+  }
+
+
+
+
+  var jQuery = require('jQuery');
+
+  function Image(imageId) {
+    this._imageId = imageId;
+  }
+
+  Image.prototype = {
+
+    _getElement: function() {
+      return jQuery('#' + this._imageId);
+    },
+
+    show: function() {
+      this._getElement().show();
+    },
+
+    hide: function() {
+      this._getElement().hide();
+    },
+
+    rotate: function(angle) {
+      this._getElement().css('webkitTransform', 'rotate(' + angle + 'deg)');
+    }
+
+  };
+
+
+  function showAndRotateImage(imageId, angle) {
+    var image = new Image(imageId);
+    image.show();
+    image.rotate(angle);
+  }
+
+  it('should show the image', function() {
+    spyOn(Image.prototype, 'show');
+
+    showAndRotateImage('imageId', 90);
+    expect(Image.prototype.show)
+      .toHaveBeenCalled();
+  });
+
+
+
