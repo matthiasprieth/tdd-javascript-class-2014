5da0742f2a84d26fac8dfd4cfa530eefdf65b0ef
Wolfram Kriesing <wk@uxebu.com>
2014-04-08 16:56:48 +0200

    First test and implementation.

--- src/mockExampleSpec.js
+++ src/mockExampleSpec.js
@@ -4,4 +4,13 @@
 - rotate image clockwise 90º for every full page height scrolled down
  */
 
+ddescribe('rotate depending on the scroll offset', function() {
+  it('scroll is 0, rotate by 0 percent', function() {
+    expect(getAngleByScrollOffset(0))
+      .toBe(0);
+  });
+});
 
+function getAngleByScrollOffset() {
+  return 0;
+}
