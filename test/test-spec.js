400e93e120f2625f914a2a041ba17cbcbe84bd09
Matthias Prieth <mprieth.mmt-b2011@fh-salzburg.ac.at>
2014-04-11 18:22:44 +0200

    first test: getDegree

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,6 +1,11 @@
-require('jasmine-matchers');
-describe("Hello world", function() {
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+var jQuery = require('jquery');
+
+function getDegree() {
+    return "Nord"
+}
+
+describe("Compass", function() {
+    it("compass should show nord", function() {
+        expect(getDegree()).toEqual("Nord");
     });
 });
