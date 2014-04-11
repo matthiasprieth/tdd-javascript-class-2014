9715b3a41277cd4da332e0dfa0482d76e8f9ed7a
Matthias Prieth <mprieth.mmt-b2011@fh-salzburg.ac.at>
2014-04-11 18:38:53 +0200

    compass should show South, compass should show West

--- test/test-spec.js
+++ test/test-spec.js
@@ -7,6 +7,10 @@ function getDegree() {
            return 'Nord';
          case 90:
            return 'East';
+        case 180:
+           return 'South';
+        case 270:
+           return 'West';
     }
 }
 
@@ -14,9 +18,16 @@ describe("Compass", function() {
     it("compass should show Nord", function() {
         expect(getDegree()).toEqual("Nord");
     });
-
     it("compass should show East", function() {
         degree += 90;
         expect(getDegree()).toEqual("East");
+    });
+    it("compass should show South", function() {
+        degree += 90;
+        expect(getDegree()).toEqual("South");
+    });
+    it("compass should show West", function() {
+        degree += 90;
+        expect(getDegree()).toEqual("West");
     });
 });
