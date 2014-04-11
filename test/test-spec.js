b83650950398bac9486b6de8299236e5b0ab93d0
Matthias Prieth <mprieth.mmt-b2011@fh-salzburg.ac.at>
2014-04-11 20:04:13 +0200

    compass should check if out of range

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,6 +1,8 @@
 var jQuery = require('jquery');
 
-cardinalPoints ={
+var degree = 0;
+/*
+cardinalPoints = {
     0:'North',
     45:'North-East',
     90:'East',
@@ -10,19 +12,60 @@ cardinalPoints ={
     270:'West',
     315:'North-West'
 }
-function getDegree(degree) {
-    return cardinalPoints[degree];
+*/
+function getDegree() {
+    switch(degree) {
+        case 0:
+            return 'North';
+        case 45:
+            return 'North-East';
+        case 90:
+            return 'East';
+        case 135:
+            return 'South-East';
+        case 180:
+            return 'South';
+        case 225:
+            return 'South-West';
+        case 270:
+            return 'West';
+        case 315:
+            return 'North-West';
+    }
+    if(degree >= 360 || degree > 0){
+        degree = 0;
+        return 'North';
+    }else{
+        return degree;
+    }
 }
 
 describe("Compass", function() {
     it("compass should show cardinal points", function() {
-        expect(getDegree(0)).toEqual("North");
-        expect(getDegree(45)).toEqual("North-East");
-        expect(getDegree(90)).toEqual("East");
-        expect(getDegree(135)).toEqual("South-East");
-        expect(getDegree(180)).toEqual("South");
-        expect(getDegree(225)).toEqual("South-West");
-        expect(getDegree(270)).toEqual("West");
-        expect(getDegree(315)).toEqual("North-West");
+        expect(getDegree()).toEqual("North");
+        degree += 45;
+        expect(getDegree()).toEqual("North-East");
+        degree += 45;
+        expect(getDegree()).toEqual("East");
+        degree += 45;
+        expect(getDegree()).toEqual("South-East");
+        degree += 45;
+        expect(getDegree()).toEqual("South");
+        degree += 45;
+        expect(getDegree()).toEqual("South-West");
+        degree += 45;
+        expect(getDegree()).toEqual("West");
+        degree += 45;
+        expect(getDegree()).toEqual("North-West");
+        degree += 45;
     });
+    it("compass should check if out of range", function() {
+        for (var i=360;i<=370;i++){
+            expect(getDegree(i)).toEqual('North');
+        }
+        for (var i=0;i>=-10;i--){
+            expect(getDegree(i)).toEqual('North');
+        }
+    });
+
 });
